539f1e68ed7210d83bc0a2b313585c3d8994ba88
rixfxol <54469435+rixfxol@users.noreply.github.com>
2024-04-20 08:41:43 +0800

    refactor: autoinserted project packages for preemptive packaging setup

---
+++ Source/Modules/Core/officore.py
@@ -0,0 +1,19 @@
+# This module contains the definitions for the core program of offi,
+# which handles the transactions  
+
+
+#   
+class officore:
+    # Constructors and Specials
+    def __init__(core):
+        pass
+
+    # Private Methods
+    
+
+
+    # Test Emulated Transaction
+    def test(core):
+        pass
+    
+    
---
+++ Source/main.py
@@ -0,0 +1,10 @@
+# This module contains the main function and the
+# launch point for the program.
+
+from Modules.Core import officore as ofc
+
+def main():
+    init = ofc.officore()
+
+if __name__ == '__main__':
+    main()
