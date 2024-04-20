e357e45ef44a96ffec41b36c1ddda890b0ebe4b6
rixfxol <54469435+rixfxol@users.noreply.github.com>
2024-04-20 09:21:33 +0800

    feat: instantiated officore and added offi module files

--- Source/Modules/Core/officore.py
+++ Source/Modules/Core/officore.py
@@ -1,15 +1,39 @@
 # This module contains the definitions for the core program of offi,
 # which handles the transactions  
 
+# Packages
+
+# Project Modules
 
 #   
 class officore:
     # Constructors and Specials
-    def __init__(core):
-        pass
+    def __init__(core, args):
+        # Boot Core
+        core.__load_glob()
+        if len(args): core.__parse_args(args)
+        if core.__run_mode == 'cmd':
+            print(f"Offi {str.capitalize(core.__version_stage)} {core.__version}\n")
+            print("Development Test Version")
+        # Prompt
+        core.__page_prompt()
 
     # Private Methods
-    
+    def __load_glob(core):
+        # Temporary Global Values, make read function later
+        core.__version = '0.0.a1'
+        core.__version_stage = 'alpha'
+        core.__run_mode = 'cmd'
+
+    def __page_prompt(core):
+        # CMD 
+        if core.__run_mode == 'cmd':
+            print()
+
+    def __parse_args(core, args):
+        pass
+
+    # Public Methods
 
 
     # Test Emulated Transaction
--- Source/main.py
+++ Source/main.py
@@ -1,10 +1,15 @@
 # This module contains the main function and the
 # launch point for the program.
 
+# Packages
+import sys
+
+# Project Modules
 from Modules.Core import officore as ofc
 
+# Main Function
 def main():
-    init = ofc.officore()
+    init = ofc.officore(sys.argv[1:])
 
 if __name__ == '__main__':
     main()
