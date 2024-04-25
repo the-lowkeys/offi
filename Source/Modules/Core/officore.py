df9d90c237d767866d89ffed1b8e3b14a584ce22
rixfxol <54469435+rixfxol@users.noreply.github.com>
2024-04-25 22:37:31 +0800

    feat: reorganized folders and pre-testing of framework

--- Source/Modules/Core/officore.py
+++ Source/Modules/Core/officore.py
@@ -6,22 +6,28 @@ import flet as ft
 
 # Project Modules
 from ..Database import offidb as db
-
-from ...Pages.LoginDashboard import logdash as lgd
+"""
+from ...Pages.LoginDashboard.logdash import logdash as lgd
 from ...Pages.RequestDashboard import reqdash as rqd
 from ...Pages.AdminDashboard import adidash as amd
 from ...Pages.ValidationDashboard import valdash as vld
-
+"""
 # Core Class
 class offi_core:
-    def __init__(core, args, page):
-        # Exception Cascade
+    # Constructor
+    def __init__(core, args):
+        # Protection Clause
         if type(args) is not list:
-            raise TypeError("Argument for parameter \'args\' is not an argument list!")
+            raise TypeError("Argument for parameter \'args\' is not of type \'list\'!")
+        
+    def __args_mod(core, args):
         pass
+        
 
 # Helper Functions
 def switch_view(view):
-    pass
+    # Protection Clause
+    if type(view) is not ft.View:
+        raise TypeError("Argument for parameter \'view\' is not of type \'flet.View\'!")
     
     
