c736e6f112709c179a552cf3ceef3f04ba55e98e
Vyrik554 <recuencocyril@gmail.com>
2024-05-09 22:49:00 +0800

    temp: employee id box adjusted to red

--- components/pages/login.py
+++ components/pages/login.py
@@ -24,6 +24,7 @@ def draw(page, debug=False, handlers: tuple=None):
     text_username: TextField = TextField(label='Employee ID', text_align=ft.TextAlign.LEFT, width=500)
     text_password: TextField = TextField(label='Password', text_align=ft.TextAlign.LEFT, width=500, password=True)
     button_submit: ElevatedButton = ElevatedButton(text='Login', width=200, height=75, disabled=True)
+    checkbox_show_password: Checkbox = Checkbox(label='Show Password', value=False)
     
     def validate(e:ControlEvent) ->None:
         if text_username.value.isdigit() and len(text_username.value) <= 10:
@@ -33,9 +34,13 @@ def draw(page, debug=False, handlers: tuple=None):
                 button_submit.disabled = True
         else:
             text_username.value = ''
+            text_username.border_color = 'red'
             print('Invalid input. Please enter up to 10 digits.')
         page.update()
-
+        
+    def toggle_password_visibility(e:ControlEvent) -> None:
+        text_password.password = not checkbox_show_password.value
+        page.update()
 
     def submit(e:ControlEvent) -> None:
         print('Username:', text_username.value)
@@ -44,6 +49,7 @@ def draw(page, debug=False, handlers: tuple=None):
 
     text_username.on_change = validate
     text_password.on_change = validate
+    checkbox_show_password.on_change = toggle_password_visibility
     #button_submit.on_click = submit
     
     page.add(
@@ -60,11 +66,17 @@ def draw(page, debug=False, handlers: tuple=None):
     )
     page.add(
         Row(
-            controls=[button_submit],
+            controls=[checkbox_show_password],
             alignment=ft.MainAxisAlignment.CENTER
         )
     )
 
+    page.add(
+        Row(
+            controls=[button_submit],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
     
 
 
