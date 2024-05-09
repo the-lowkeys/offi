de80e339a8c08c20ff64d22651d503729b26c4d4
Vyrik554 <recuencocyril@gmail.com>
2024-05-09 22:13:25 +0800

    temp: adjusted shits

--- components/pages/login.py
+++ components/pages/login.py
@@ -13,9 +13,7 @@ def draw(page, debug=False, handlers: tuple=None):
     # Type Check
     if type(page) != ft.Page:
         raise TypeError('Argument \'page\' is not an \'flet.Page\' object!')
-    if type(handlers) != None:
-        if type(handlers) != tuple:
-            raise TypeError('Argument \'handlers\' is not of type \'tuple\'')
+    
     
     # Draw Screen
     page.title = 'OFFI'
