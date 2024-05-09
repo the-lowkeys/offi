149b3864c90ab26e4e01533ab5d356dc5a41d38f
Vyrik554 <recuencocyril@gmail.com>
2024-05-09 21:38:02 +0800

    temp: adjusted login button size and removed box

--- components/pages/login.py
+++ components/pages/login.py
@@ -15,17 +15,19 @@ def draw(page, debug=False):
         raise TypeError('Argument \'page\' is not an \'flet.Page\' object!')
     
     # Draw Screen
-    page.title = 'Login'
+    page.title = 'OFFI'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.theme_mode = ft.ThemeMode.DARK
-    page.window_width = 500
-    page.window_height = 500    
+    page.window_width = 700
+    page.window_height = 700    
     page.window_resizable = True
+    txt = ft.Text(value="Welcome to OFFI", text_align=ft.TextAlign.CENTER, width=500, size=50)
+    page.add(txt)
     
-    text_username: TextField = TextField(label='Username', text_align=ft.TextAlign.LEFT, width=200)
-    text_password: TextField = TextField(label='Password', text_align=ft.TextAlign.LEFT, width=200, password=True)
-    checkbox_signup: Checkbox = Checkbox(label='i agree to this cresidentials', value=False)
-    button_submit: ElevatedButton = ElevatedButton(text='submit', width=200, disabled=True)
+    text_username: TextField = TextField(label='Username', text_align=ft.TextAlign.LEFT, width=500)
+    text_password: TextField = TextField(label='Password', text_align=ft.TextAlign.LEFT, width=500, password=True)
+    button_submit: ElevatedButton = ElevatedButton(text='Login', width=200, height=75, disabled=True)
     
     def validate(e:ControlEvent) ->None:
         if all([text_username.value, text_password.value]):
@@ -47,24 +49,29 @@ def draw(page, debug=False):
             )
         )
 
-    checkbox_signup.on_change = validate
     text_username.on_change = validate
     text_password.on_change = validate
     button_submit.on_click = submit
     
     page.add(
         Row(
-            controls=[
-                Column(
-                    [text_username,
-                     text_password,
-                     checkbox_signup,
-                     button_submit]
-                )
-            ],
+            controls=[text_username],
             alignment=ft.MainAxisAlignment.CENTER
         )
     )
+    page.add(
+        Row(
+            controls=[text_password],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+    page.add(
+        Row(
+            controls=[button_submit],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
     
 
 
